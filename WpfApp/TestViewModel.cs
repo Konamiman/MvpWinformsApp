05ac85546a93e83de2c5c4069daca06d366945b5
Néstor Soriano <konamiman@konamiman.com>
2015-12-11 10:20:53 +0100

    Borrado un proyecto WPF que no tiene nada que ver y fue incluído por error.

--- WpfApp/TestViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿public class TestViewModel
-{
-    public double ValueNotNull { get; set; }
-    public double? NulleableValue { get; set; }
-
-    public double? UserControlNulleableValue { get; set; }
-
-    public bool Factor { get; set; }
-}
